19a00403bb20dafc00a64ede20fce746a30ddba3
Alisson Barbosa <161394750+Alysson-b@users.noreply.github.com>
2025-01-25 03:38:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], 
-    // origin: '*', 
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY', 'user-id'], 
   },
